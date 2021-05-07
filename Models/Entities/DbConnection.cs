20682dd0239675231099d470fc6030e23bed558d
QuanPhung <quanbka00@gmail.com>
2021-05-07 08:57:26 +0700

    update project soccer app

--- Models/Entities/DbConnection.cs
+++ Models/Entities/DbConnection.cs
@@ -2,6 +2,6 @@ namespace SoccerManageApp.Models.Entities.Models
 {
     public static class DbConnection
     {
-       public static string connectionString = "Server=localhost;Port=5432;User ID=postgres;Password=10180401;Database=PF_SoccerManageApp;Pooling=True;";
+       public static string connectionString = "Server=localhost;Port=5432;User ID=postgres;Password=crquan07;Database=soccer_manage_db;Pooling=True;";
     }
 }
